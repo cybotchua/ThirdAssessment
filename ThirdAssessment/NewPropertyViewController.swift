c455a336b05ec7b59c6498730b718dfbe73a48e7
Terence Chua <Terence@Terences-MacBook-Pro.local>
2018-02-02 17:09:12 +0800

    added add & edit

--- ThirdAssessment/NewPropertyViewController.swift
+++ ThirdAssessment/NewPropertyViewController.swift
@@ -9,18 +9,76 @@
 import UIKit
 
 class NewPropertyViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var priceTextField: UITextField!
+    
+    @IBOutlet weak var locationTextField: UITextField!
+    
+    var addMode : Bool = true
+    
+    var selectedProperty : Property?
+    var selectedOwner2 : Owner?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.title = "Add Property"
+        
+        if let property = selectedProperty {
+            addMode = false
+            nameTextField.text = property.name
+            priceTextField.text = String(property.price)
+            locationTextField.text = property.location
+        }
+        
+        if addMode {
+            navigationItem.title = "Add Property"
+        } else {
+            navigationItem.title = "Edit Property"
+        }
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(doneButtonPressed))
     }
     
     @objc func doneButtonPressed() {
-        guard let vcToPropertiesTableVC = storyboard?.instantiateViewController(withIdentifier: "PropertiesTableViewController") as? PropertiesTableViewController else {return}
+
+        if let name = nameTextField.text,
+            let priceText = priceTextField.text,
+            let location = locationTextField.text,
+            name != "",
+            priceText != "",
+            location != "",
+            let price = Int16(priceText) {
+            
+            if addMode {
+                createProperty(name: name, price: price, location: location)
+            } else {
+                updateProperty(name: name, price: price, location: location)
+            }
+            
+        }
         
         navigationController?.popViewController(animated: true)
     }
-
-
+    
+    func createProperty(name: String, price: Int16, location: String) {
+        let newProperty = Property(entity: Property.entity(), insertInto: DataController.moc)
+        newProperty.name = name
+        newProperty.price = price
+        newProperty.location = location
+        selectedOwner2?.addToOwnsProperty(newProperty)
+        
+        DataController.saveContext()
+    }
+    
+    func updateProperty(name: String, price: Int16, location: String) {
+        guard let property = selectedProperty else {return}
+        property.name = name
+        property.price = price
+        property.location = location
+        
+        DataController.saveContext()
+    }
+    
+    
 }
--- ThirdAssessment/PropertiesTableViewController.swift
+++ ThirdAssessment/PropertiesTableViewController.swift
@@ -7,41 +7,74 @@
 //
 
 import UIKit
+import CoreData
 
 class PropertiesTableViewController: UIViewController {
     
     @IBOutlet weak var propertiesTableView: UITableView!
     
-
     var selectedOwner : Owner?
+    
+    var propertyName : String = ""
+    var propertyPrice : Int16 = 0
+    var propertyLocation : String = ""
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         propertiesTableView.dataSource = self
+        propertiesTableView.delegate = self
         
         navigationItem.title = "Properties"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(addButtonTapped))
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        propertiesTableView.reloadData()
+    }
+    
     
     @objc func addButtonTapped() {
         guard let vcToNewProperty = storyboard?.instantiateViewController(withIdentifier: "NewPropertyViewController") as? NewPropertyViewController else {return}
         
-//        vcToNewProperty.product2 = selectedProduct
+        vcToNewProperty.selectedOwner2 = selectedOwner
         
         navigationController?.pushViewController(vcToNewProperty, animated: true)
     }
-
-
+    
+    
 }
 
-extension PropertiesTableViewController : UITableViewDataSource {
+extension PropertiesTableViewController : UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return selectedOwner?.ownsProperty?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = propertiesTableView.dequeueReusableCell(withIdentifier: "propertyCell", for: indexPath)
+        
+        if let properties = selectedOwner?.ownsProperty?.allObjects as? [Property] {
+            let currentProperty = properties[indexPath.row]
+            cell.textLabel?.text = currentProperty.name
+            cell.detailTextLabel?.text = "\(currentProperty.price), \(currentProperty.location ?? "n/a")"
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let sb = UIStoryboard(name: "Main", bundle: Bundle.main)
+        if let vcToNewProperty = sb.instantiateViewController(withIdentifier: "NewPropertyViewController") as? NewPropertyViewController {
+            
+            if let properties = selectedOwner?.ownsProperty?.allObjects as? [Property] {
+                vcToNewProperty.selectedProperty = properties[indexPath.row]
+                navigationController?.pushViewController(vcToNewProperty, animated: true)
+
+            }
+        }
     }
 }
+
+
+

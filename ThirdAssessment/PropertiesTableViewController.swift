c4fc8a3ba3384c1d74c7c7cd620d15d1be9080a3
Terence Chua <Terence@Terences-MacBook-Pro.local>
2018-02-02 13:03:32 +0800

    set tableView for propertiesTableViewController & set navigation item

--- ThirdAssessment/PropertiesTableViewController.swift
+++ ThirdAssessment/PropertiesTableViewController.swift
@@ -9,30 +9,35 @@
 import UIKit
 
 class PropertiesTableViewController: UIViewController {
+    
+    @IBOutlet weak var propertiesTableView: UITableView!
+    
 
     var selectedOwner : Owner?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        propertiesTableView.dataSource = self
+        
+        navigationItem.title = "Properties"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(addButtonTapped))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func addButtonTapped() {
+        
+    }
+
+
+}
+
+extension PropertiesTableViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
-    */
-
 }

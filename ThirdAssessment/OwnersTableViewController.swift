394559ee5249c6fa8688d6cf81a6aee9bfbc5d4d
Terence Chua <Terence@Terences-MacBook-Pro.local>
2018-02-02 12:27:13 +0800

    Property Owners Loaded

--- ThirdAssessment/OwnersTableViewController.swift
+++ ThirdAssessment/OwnersTableViewController.swift
@@ -10,31 +10,82 @@ import UIKit
 import CoreData
 
 class OwnersTableViewController: UIViewController {
-
+    
     @IBOutlet weak var ownersTableView: UITableView!
     
     var fetchResultController = NSFetchedResultsController<Owner>()
-
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         ownersTableView.dataSource = self
-        fetchResultController.delegate = self
-
+        ownersTableView.delegate = self
+        if !UserDefaults.standard.bool(forKey: "alreadyFirstRun") {
+            loadOwners()
+            fetchOwners()
+            UserDefaults.standard.set(true, forKey: "alreadyFirstRun")
+        } else {
+            fetchOwners()
+        }
     }
-
-
-
+    
+    func loadOwners() {
+        let owners = ["You Jing", "Faris Roslan", "Ming Xiang", "Josh Teng", "Jalen Ong", "Shin Yin", "Tommy MiddleFinger", "Yen Ping", "Estelle Chang", "Audrey Ling"]
+        for item in owners {
+            guard let owner = NSEntityDescription.insertNewObject(forEntityName: "Owner", into: DataController.moc) as? Owner else {return}
+            
+            owner.name = item
+        }
+        DataController.saveContext()
+        ownersTableView.reloadData()
+    }
+    
+    func fetchOwners() {
+        let request = NSFetchRequest<Owner>(entityName: "Owner")
+        
+        let sortName = NSSortDescriptor(key: "name", ascending: true)
+        request.sortDescriptors = [sortName]
+        
+        fetchResultController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: DataController.moc, sectionNameKeyPath: nil, cacheName: nil)
+        fetchResultController.delegate = self
+        
+        do {
+            try fetchResultController.performFetch()
+            ownersTableView.reloadData()
+        } catch {
+            print("Error Fetching Owner Data")
+        }
+        
+    }
+    
+    
+    
 }
 
-extension OwnersTableViewController : UITableViewDataSource {
+extension OwnersTableViewController : UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return fetchResultController.fetchedObjects?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ownersCell", for: indexPath)
+        let currentCell = fetchResultController.object(at: indexPath)
+        
+        cell.textLabel?.text = currentCell.name
+        
+        return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let sb = UIStoryboard(name: "Main", bundle: Bundle.main)
+        if let vc = sb.instantiateViewController(withIdentifier: "PropertiesTableViewController") as? PropertiesTableViewController {
+            vc.selectedOwner = fetchResultController.object(at: indexPath)
+            
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
 }
 
 extension OwnersTableViewController : NSFetchedResultsControllerDelegate {
@@ -45,8 +96,8 @@ extension OwnersTableViewController : NSFetchedResultsControllerDelegate {
     
     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
         print("DidChange")
-        print("old : ", indexPath)
-        print("new : ", newIndexPath)
+        //        print("old : ", indexPath)
+        //        print("new : ", newIndexPath)
         
         switch type {
         case .insert:
